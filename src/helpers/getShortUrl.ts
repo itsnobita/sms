b6106297cfcb3c86fc7bd226842283348360bb12
NAVEEN GOYAL -X (navgoyal - HCL TECHNOLOGIES LIMITED at Cisco) <navgoyal@cisco.com>
2024-07-21 05:41:11 +0530

    fix

--- src/helpers/getShortUrl.ts
+++ src/helpers/getShortUrl.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
 export async function getShortURL(_id: string, name: string) {
-    try {
-      const response = await axios.post(`${process.env.SHORTURL_CREATE}`, {
+  try {
+    const response = await axios.post(
+      `${process.env.SHORTURL_CREATE}`,
+      {
         long_url: `${process.env.BASEURL}/${_id}`,
         // expire_at_datetime: "2035-01-17 15:00:00",
         description: `Send ${name} a annonymous message by Secret Message Sender`,
-      });
-        console.log(response)
-        return response.data.short_url;
-    } catch (error) {
-        throw error
-    }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.SHORTURL_KEY}`,
+        },
+      }
+    );
+    console.log(response);
+    return response.data.short_url;
+  } catch (error) {
+    throw error;
   }
+}
